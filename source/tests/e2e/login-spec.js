c54ff711a0f1f9188fb1e1390c39d3c28b386dfc
Vladyslav Tkachenko <Vladyslav.Tkachenko@harmonicinc.com>
2019-10-04 18:27:42 +0300

    login-spec.js refactoring for commit it to master branch

--- source/tests/e2e/login-spec.js
+++ source/tests/e2e/login-spec.js
@@ -28,53 +28,16 @@ describe('Login Page Tests', function () {
         loginPage.password.sendKeys('demo');
         expect((loginPage.password.getAttribute('value'))).toEqual('demo');
     });
-    /*
 
 
-    xit('Click Login', function () {
-        loginPage.submitButton.click();
-        expect(browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000));
-        expect(loginPage.notices.isDisplayed());
-        let mytext = loginPage.notices.getText()
-            .then((text) => {
-                console.log(text);
-                mytext = text;
-                return mytext
-            })
-            .catch((error) => console.log(error));
-        console.log('My log: ', mytext);
-        expect(mytext.trim()).toEqual('The user could not be found in our database');
-    });
- */
     it('Click Login', async function () {
         loginPage.submitButton.click();
         expect(browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000));
         expect(loginPage.notices.isDisplayed());
-       // console.log(await element.all(by.id('notices').get().count()));
-      //  browser.sleep(5000);
+        let alertText = await loginPage.notices.getText();
+        console.log(alertText);
+        expect(alertText.trim()).toContain('The user could not be found in our database')
 
-        await loginPage.notices.child()
-
-        //    let myText = await loginPage.notices.map(elementFinder => elementFinder.getText());
-        //  console.log(myText);
-        //  browser.sleep(100000);
-        ///  expect(myText.trim()).toEqual('The user could not be found in our database');
-    });
-
-
-    xit('Set wrong login', () => {
-        loginPage.userName.clear();
-        loginPage.userName.sendKeys('demo111');
-        expect((loginPage.userName.getAttribute('value'))).toEqual('demo111');
-    });
-
-    xit('Click Login', function () {
-        loginPage.submitButton.click();
-        expect(browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000));
-        expect(loginPage.notices.isDisplayed());
-        loginPage.notices.getText()
-            .then((text) => console.log(text));
-        browser.sleep(100000);
     });
 
     afterEach(function () {
