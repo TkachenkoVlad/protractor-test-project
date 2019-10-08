cf8bbb4214c52ce7ba54737918428f0c1d6b349a
Vladyslav Tkachenko <tkachenko94ua@gmail.com>
2019-10-08 10:34:25 +0300

    login-spec.js add few cases to spec

--- source/tests/e2e/login-spec.js
+++ source/tests/e2e/login-spec.js
@@ -1,7 +1,6 @@
 'use strict';
 let LoginPage = require('../../pages/login-page');
 
-
 describe('Login Page Tests', function () {
     let loginPage;
 
@@ -35,15 +34,41 @@ describe('Login Page Tests', function () {
         expect((loginPage.password.getAttribute('value'))).toEqual('demo');
     });
 
-
-    it('Click Login', async function () {
+    it('Click "Login" button(first time)', async function () {
         loginPage.submitButton.click();
-        expect(browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000));
-        expect(loginPage.notices.isDisplayed());
+        browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000);
         let alertText = await loginPage.notices.getText();
         console.log(alertText);
         expect(alertText.trim()).toContain('The user could not be found in our database')
+    });
 
+    it('Set correct login', () => {
+        loginPage.userName.clear();
+        loginPage.userName.sendKeys('demo');
+        expect((loginPage.userName.getAttribute('value'))).toEqual('demo');
+    });
+
+    it('Set wrong password', () => {
+        loginPage.password.clear();
+        loginPage.password.sendKeys('1111');
+        expect((loginPage.password.getAttribute('value'))).toEqual('1111');
+    });
+
+    it('Click "Login" button(second time)', async function () {
+        loginPage.submitButton.click();
+        browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000);
+        let alertText = await loginPage.notices.getText();
+        console.log(alertText);
+        expect(alertText.trim()).toContain('The user could not be found in our database')
+    });
+
+    it('Click "Login" button(third time)', async function () {
+        loginPage.submitButton.click();
+        browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000);
+        let alertText = await loginPage.notices.getText();
+        console.log(alertText);
+        expect(alertText.trim()).toContain("You have * login attempts left until your account is temporary blocked");
+        expect(alertText.trim()).toContain('Wrong combination of username and password or the account does not exist.');
     });
 
     afterEach(function () {
@@ -51,3 +76,7 @@ describe('Login Page Tests', function () {
     })
 });
 
+/*
+expect(browser.wait(ExpectedConditions.visibilityOf(loginPage.notices), 5000));
+        expect(loginPage.notices.isDisplayed());
+ */
